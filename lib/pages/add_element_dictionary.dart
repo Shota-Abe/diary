ca5a83018cbde5c731510fdb1af44b557febd3b5
Gako <gakochan415@gmail.com>
2025-08-27 22:52:51 +0900

    image_pickwerの設定をadd_elementページとpubspec.yamlに追加しました。

--- lib/pages/add_element_dictionary.dart
+++ lib/pages/add_element_dictionary.dart
@@ -1,10 +1,53 @@
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class AddElementDictionary extends StatelessWidget {
   //図鑑の要素を追加するページ
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text('アクティビティを追加')));
+    return Scaffold(
+      appBar: AppBar(title: Text('アクティビティを追加')),
+
+      body: Container(
+        width: double.infinity,
+        child: Column(
+          children: [
+            Container(
+              padding: const EdgeInsets.all(8.0), // 内側に余白を持たせると見栄えが良くなります
+              decoration: BoxDecoration( // 枠線などをつける場合
+                border: Border.all(color: Colors.grey),
+                borderRadius: BorderRadius.circular(8.0),
+              ),
+              child:TextField(
+                decoration: InputDecoration(
+                  hintText: 'アクティビティ'
+                ),
+              ),
+            ),
+
+            Container(
+              padding: const EdgeInsets.all(8.0), // 内側に余白を持たせると見栄えが良くなります
+              decoration: BoxDecoration( // 枠線などをつける場合
+                border: Border.all(color: Colors.grey),
+                borderRadius: BorderRadius.circular(8.0),
+              ),
+              child:TextField(
+                decoration: InputDecoration(
+                  hintText: '説明'
+                ),
+              ),
+            ),
+
+            ElevatedButton(
+              child: Text('新規登録'),
+              onPressed: (){
+                
+              }, 
+            )
+          ],
+        )
+      )
+    );
   }
 }
